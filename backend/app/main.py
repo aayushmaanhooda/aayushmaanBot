42776b94073f70b7d6a1af4a47ad2d198994acd6
aayushmaanhooda <aayushmaan162@gmail.com>
2026-02-21 16:35:14 +1100

    added ssl , ngnix to ec2 and updated urls

--- backend/app/main.py
+++ backend/app/main.py
@@ -22,7 +22,10 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
-    CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"]
+    CORSMiddleware,
+    allow_origins=["https://aayushmaan-bot.vercel.app", "http://localhost:5173", "https://aayushbot.myddns.me"],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
@@ -59,4 +62,4 @@ async def chat(req: ChatRequest):
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("main:app", host="localhost", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
