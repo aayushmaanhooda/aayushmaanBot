3be320a2840cd82ddbb6c9defaa0c3614d8ee587
aayushmaanhooda <aayushmaan162@gmail.com>
2026-03-05 17:30:21 +1100

    updated security keep issue

--- backend/app/security.py
+++ backend/app/security.py
@@ -1,9 +1,13 @@
 from fastapi import Security, HTTPException
 from fastapi.security.api_key import APIKeyHeader
 import os
+from dotenv import load_dotenv
 
-API_KEY = os.getenv("API_KEY")
+load_dotenv()
+
+API_KEY = os.getenv("SECURITY_API_KEY")
 API_KEY_NAME = "x-api-key"
+print("KEY", API_KEY)
 
 api_key_header = APIKeyHeader(name=API_KEY_NAME, auto_error=False)
 
